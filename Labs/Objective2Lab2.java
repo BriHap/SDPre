7380fe6bc0a854bf156e592409a688d5692c18ee
Brian <haptonstall_b@yahoo.com>
2025-05-22 12:16:52 -0600

    finish Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+	public static void main (String[] args) {
+	  String name = "Brian Haptonstall";
+      int age = 52;
+      String state = "Colorado";
+
+
+	System.out.println("Name: Brian Haptonstall Age: 52 State: Colorado");
+
+}
+
+	}
