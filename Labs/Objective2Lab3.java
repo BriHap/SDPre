304ae032190de0c47fbc9d974ca70e556a8f5e4f
Brian <haptonstall_b@yahoo.com>
2025-05-22 13:36:41 -0600

    finish Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+    int year = 1972;
+    String month = "July";
+    boolean weekend = false;
+
+    System.out.println(year);
+    System.out.println(month);
+    System.out.println(weekend);
+  }
+}
