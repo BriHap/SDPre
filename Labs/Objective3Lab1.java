ce7d4f9ec3423f6f6ccbf0f9f47971fd81e35050
Brian <haptonstall_b@yahoo.com>
2025-05-23 16:11:23 -0600

    finish Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,16 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    
+
+    double num1 = 22, num2 = 44, num3 = 52, average = 39.333;
+
+    
+
+  
+    System.out.println(average);
+
+
+
+
+  }
+}
