e81b065078bcc3ca1ce70acf97307031fa1eb21e
Brian <haptonstall_b@yahoo.com>
2025-05-23 18:32:43 -0600

    finish Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,12 @@
+public class Objective3Lab2 {
+   public static void main(String[] args) {
+
+   	double side1 = Math.sqrt(7 * 7), side2 = Math.sqrt(9 * 9); 
+      double hypotenuse = 11.402;
+
+   	System.out.println(hypotenuse);
+
+
+   	 }
+
+   		}
