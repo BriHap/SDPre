01d622a0d368022f6218d179d84acd9f9df533df
Brian <haptonstall_b@yahoo.com>
2025-05-24 07:43:42 -0600

    finish Objective3Lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,15 @@
+public class Objective3Lab3 {
+	public static void main(String[] args) {
+
+	int first = 1 + (int)(Math.random() * 6);
+	int second = 1 + (int)(Math.random() * 6);
+	int third = first + second;
+	int sum = third;
+
+
+;	
+	System.out.println(sum);
+
+}
+
+	}
